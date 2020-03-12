9f55059c88b798160e6c3b788bdd17937015ad12
Maui Torio <mauiandsonsph@yahoo.com>
2020-03-12 21:32:09 +0800

    added necessary files to finish

---
+++ public/js/custom.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    // $("#calendar").fullCalendar("rerenderEvents");
+    // $("#submitBtn").click(function(event) {
+    //     $("#alert")
+    //         .append(
+    //             `<div class="alert alert-success" role="alert">
+    //     A simple secondary alert—check it out!
+    //   </div>`
+    //         )
+    //         .fadeOut();
+    // });
+});
